f7506a3b13d6a78ff314e4696c0ebd9aecfbd63c
Fernando Giminski <fernandogiminski@gmail.com>
2025-08-02 17:52:26 -0300

    agregado de mas cosas

--- Entrega1/settings.py
+++ Entrega1/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #'myapp_entrega1',
+    'Main',
     'blog',
+
 ]
 
 MIDDLEWARE = [
--- Entrega1/urls.py
+++ Entrega1/urls.py
@@ -15,13 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 #from myapp_entrega1 import views
-from blog import views
+
 
 urlpatterns = [
-    #path('admin/', admin.site.urls),
-    path('blog/', views.post_list, name='post_list'),
-  
+    path('admin/', admin.site.urls),
+    path("", include("Main.urls")),
+    path("", include("blog.urls")),
 ]
+
