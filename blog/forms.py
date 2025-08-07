2ba73ad4cead534015d627601ed909e37f2e9f3b
Fernando Giminski <fernandogiminski@gmail.com>
2025-08-07 09:26:55 -0300

    actualizacion

--- blog/forms.py
+++ blog/forms.py
@@ -5,4 +5,14 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['titulo', 'contenido']
-
+        widgets = {
+            'titulo': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Ingresá el título de tu obra',
+            }),
+            'contenido': forms.Textarea(attrs={
+                'class': 'form-control',
+                'rows': 6,
+                'placeholder': 'Escribí una descripción o lo que quieras compartir...',
+            }),
+        }
