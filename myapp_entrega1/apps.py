05d393367d1d4b87ebaef65a036ec31ab63099b6
Fernando Giminski <fernandogiminski@gmail.com>
2025-08-07 11:24:10 -0300

    Limpieza de entorno virtual y eliminación de app antigua

--- myapp_entrega1/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MyappEntrega1Config(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'myapp_entrega1'
--- myapp_entrega1/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def index(request):
-    context = {"mensaje": "Hola, mundo!"}
-    return render(request, 'myapp_entrega1/index.html', context)
